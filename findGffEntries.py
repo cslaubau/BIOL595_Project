cdc01625e69aaa197611f2e704f67f9cfe03be46
Curtis-Mac <cslaubau@purdue.edu>
2018-04-09 15:07:53 -0400

    Started working on finding the gff entries in the target ranges. Some issue with base indexing?

--- findGffEntries.py
+++ findGffEntries.py
@@ -6,11 +6,11 @@ except IOError:
     print('There was an error opening the condensed GWAS input file')
     exit(1)
 
-gffFile = 'Triticum_aestivum.TGACv1.38.gff3'
+gffFile = 'Triticum_aestivum.TGACv1.38.txt'
 try:
     gffFh = open(gffFile, 'r')
 except IOError:
-    print('There was an error opening the gff file')
+    print('There was an error opening the gff (.txt) file')
     exit(1)
 
 # Save the ranges of interest to dictionary
@@ -21,9 +21,30 @@ for line in gwasFh:
     if chrom not in keys:
         keys.append(chrom)
         gwasRanges[chrom] = []
-    gwasRanges[chrom].append(line.split()[1:3])
+    gwasRanges[chrom].append(line.split()[2:4])
 gwasFh.close()
 
+outFile = gwasFile.replace('.cgwas', '.gff3.out')
+try:
+    outFh = open(outFile, 'w')
+except IOError:
+    print('There was an error opening the output file')
+    exit(1)
+
 # Find and save all gff ids for ranges that lie within target ranges
+for line in gffFh:
+    current = line.split()
+    if current[0] in keys:
+        [chrom, gffBegin, gffEnd, ID] = current
+        [gffBegin, gffEnd] = [int(gffBegin), int(gffEnd)]
+        # beginning or end lies between the current gwas range, save it to file
+        for entry in gwasRanges[chrom]:
+            [rangeBegin, rangeEnd] = [int(entry[0]), int(entry[1])]
+            if (gffBegin >= rangeBegin and gffBegin <= rangeEnd) or (gffEnd >= rangeBegin and gffEnd <= rangeEnd):
+                #print(entry, current)
+                pass
+                # this never happens. Is it because the gff base values are on some sort of different scale?
+                # out.write(line)
 
 gffFh.close()
+outFh.close()
